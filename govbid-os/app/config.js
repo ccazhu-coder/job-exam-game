e8ac6efec4d4d116100830f56dfde6cce2553446
ccazhu-coder <ccazhu@gmail.com>
2026-05-09 14:34:23 +0800

    chore: add shared config for API endpoint

---
+++ govbid-os/app/config.js
@@ -0,0 +1,5 @@
+window.GOVOPS_CONFIG = {
+  ENV: 'production',
+  VERSION: '2026.05.09',
+  API_URL: 'https://script.google.com/macros/s/AKfycbxpmkuK34tfjo5A3BrC9uxpn2MEyCaAHS_pNPEkHBIp46HPzaXLAg0eachrYaxzFSIoOg/exec'
+};
