e5652e9311aab75331256951dc4993b167e9f223
ccazhu-coder <ccazhu@gmail.com>
2026-05-11 12:30:08 +0800

    chore: update production API endpoint

--- govbid-os/app/config.js
+++ govbid-os/app/config.js
@@ -1,5 +1,5 @@
 window.GOVOPS_CONFIG = {
   ENV: 'production',
   VERSION: '2026.05.09',
-  API_URL: 'https://script.google.com/macros/s/AKfycbx1bhCaRf2c85JQhECQRnrLVinTIYemFd9uN4c997zzYLacNWMne_AogYq-BWyJRPlDLQ/exec'
+  API_URL: 'https://script.google.com/macros/s/AKfycbwN9rGjBfe22tYCm1DxYKfh6TgfsSfmfVOCNoaDYqsa4uPaoerQ-garvhB-jA0MzWWzLw/exec'
 };
