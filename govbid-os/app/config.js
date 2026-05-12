c92ad7f95a8681cf4cf9455c44a650d0bf579596
ccazhu-coder <ccazhu@gmail.com>
2026-05-12 16:41:05 +0800

    chore: update backend web app URL

--- govbid-os/app/config.js
+++ govbid-os/app/config.js
@@ -1,5 +1,5 @@
 window.GOVOPS_CONFIG = {
   ENV: 'production',
   VERSION: '2026.05.09',
-  API_URL: 'https://script.google.com/macros/s/AKfycbwN9rGjBfe22tYCm1DxYKfh6TgfsSfmfVOCNoaDYqsa4uPaoerQ-garvhB-jA0MzWWzLw/exec'
+  API_URL: 'https://script.google.com/macros/s/AKfycbyAcoTcQ4GYJWRmMvuzJwFrzKikerbjSsR2Pl_ca4q5Mwg0mVbjko3wAdHdLVrjgFgbKA/exec'
 };
