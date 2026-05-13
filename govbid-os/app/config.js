9f424448aa82c9231fb93df0670d20d58a87d1b4
ccazhu-coder <ccazhu@gmail.com>
2026-05-13 11:40:38 +0800

    config: 統一 API_URL 為 codegs-allinone-final 版本

--- govbid-os/app/config.js
+++ govbid-os/app/config.js
@@ -1,7 +1,7 @@
 window.GOVOPS_CONFIG = {
   ENV: 'production',
-  VERSION: '2026.05.13-commercial-launch-router-fix',
+  VERSION: '2026.05.13-codegs-allinone-final',
   APP_NAME: 'GovOps OS',
   PLAN: 'enterprise',
-  API_URL: 'https://script.google.com/macros/s/AKfycbw10pdDg_lpoeb86nU_SJfBs2kex30xPKS50pOnJD5-Z7YGPeI0MSjuJwPbd5zJbyandQ/exec'
+  API_URL: 'https://script.google.com/macros/s/AKfycbz_nUqKGMIAAcAgdJM68NV8FevV9HQ4hEs9VK5oK9PbS77xvpOHlKkR5d7NzJdgMa-fSA/exec'
 };
