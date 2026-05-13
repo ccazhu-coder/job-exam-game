97aa8b2c082966c2d72b7d6344aa41e5029a7bf8
ccazhu-coder <ccazhu@gmail.com>
2026-05-13 10:50:48 +0800

    Update production Web App URL after course ops fix

--- govbid-os/app/config.js
+++ govbid-os/app/config.js
@@ -1,7 +1,7 @@
 window.GOVOPS_CONFIG = {
   ENV: 'production',
-  VERSION: '2026.05.13-commercial-launch',
+  VERSION: '2026.05.13-commercial-launch-courseops-fix',
   APP_NAME: 'GovOps OS',
   PLAN: 'enterprise',
-  API_URL: 'https://script.google.com/macros/s/AKfycbxiO3KuKLWTmwb06GRJDMs147MavK-q7T97C0Sq5B8zU_FEJ8sLkkFhBWS6Q72g0FT9ng/exec'
+  API_URL: 'https://script.google.com/macros/s/AKfycbwdFD94QT_Fw97dK7AjdDV_Czl3ITINzYxLgMlQ58aDTjh4YXjlBNApbnev--oTxNdmBg/exec'
 };
