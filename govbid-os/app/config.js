2bdea365c7ffb7c7bfcfa534148cb18901734750
ccazhu-coder <ccazhu@gmail.com>
2026-05-13 10:32:31 +0800

    Update production Apps Script URL for commercial QA

--- govbid-os/app/config.js
+++ govbid-os/app/config.js
@@ -1,5 +1,7 @@
 window.GOVOPS_CONFIG = {
   ENV: 'production',
-  VERSION: '2026.05.13',
-  API_URL: 'https://script.google.com/macros/s/AKfycbxazsoFaFUvfAeF3hVCNXaax04qsctQrHCocwKZXKbEj4Kt6T0kKmdw7YRHybz_DOJlfg/exec'
+  VERSION: '2026.05.13-commercial-qa',
+  APP_NAME: 'GovOps OS',
+  PLAN: 'enterprise',
+  API_URL: 'https://script.google.com/macros/s/AKfycbxVfMuI1FyZmYEoaLFBz89hME_uHLrJ--86oQLYkDHS52keHlT1fdMEK3jvg_I71E27Yw/exec'
 };
