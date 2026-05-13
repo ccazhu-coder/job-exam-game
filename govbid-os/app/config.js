d3c266b714fc3adb16dadbcbeca946f44cdb8f82
ccazhu-coder <ccazhu@gmail.com>
2026-05-13 08:56:23 +0800

    Update GovOps production API URL

--- govbid-os/app/config.js
+++ govbid-os/app/config.js
@@ -1,5 +1,5 @@
 window.GOVOPS_CONFIG = {
   ENV: 'production',
-  VERSION: '2026.05.09',
-  API_URL: 'https://script.google.com/macros/s/AKfycbyAcoTcQ4GYJWRmMvuzJwFrzKikerbjSsR2Pl_ca4q5Mwg0mVbjko3wAdHdLVrjgFgbKA/exec'
+  VERSION: '2026.05.13',
+  API_URL: 'https://script.google.com/macros/s/AKfycbxazsoFaFUvfAeF3hVCNXaax04qsctQrHCocwKZXKbEj4Kt6T0kKmdw7YRHybz_DOJlfg/exec'
 };
