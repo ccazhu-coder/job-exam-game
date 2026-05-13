fb00adc2663bdb63c3cfa8e3156ffee83cd83b90
ccazhu-coder <ccazhu@gmail.com>
2026-05-13 11:01:55 +0800

    Update production URL after main router deployment

--- govbid-os/app/config.js
+++ govbid-os/app/config.js
@@ -1,7 +1,7 @@
 window.GOVOPS_CONFIG = {
   ENV: 'production',
-  VERSION: '2026.05.13-commercial-launch-courseops-fix',
+  VERSION: '2026.05.13-commercial-launch-router-fix',
   APP_NAME: 'GovOps OS',
   PLAN: 'enterprise',
-  API_URL: 'https://script.google.com/macros/s/AKfycbwdFD94QT_Fw97dK7AjdDV_Czl3ITINzYxLgMlQ58aDTjh4YXjlBNApbnev--oTxNdmBg/exec'
+  API_URL: 'https://script.google.com/macros/s/AKfycbw10pdDg_lpoeb86nU_SJfBs2kex30xPKS50pOnJD5-Z7YGPeI0MSjuJwPbd5zJbyandQ/exec'
 };
